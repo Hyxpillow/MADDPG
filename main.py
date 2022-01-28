5c02abdad48c4bb5e9b9cc9524cc46e1a87e96c9
Hyxpillow <401887202@qq.com>
2022-01-29 01:38:55 +0800

    update README.md

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import time
+import gym
+env = gym.make('CartPole-v0') #导入CartPole-v0游戏
+for episode in range(1000):  #跑Episodes幕，一幕相当于一次完整游戏
+	state = env.reset() #初始化环境，得到第一步状# 态
+	for step in range(200): #每一局游戏至多跑STEPS步
+		env.render()
+		action = 0 #强化学习算法，根据状态生成行动
+		state, reward, done, info = env.step(action) #根据action将游戏向前推进
+		# 把 (状态 奖励 动作 下一个状态) 存入 memory
+		# if step % 50 == 0:
+            # learn()
+		if done: # 如果游戏结束，则跳出step循环，进入下一episode；否则，继续step
+			break
+env.close() #结束游戏，关闭环境
