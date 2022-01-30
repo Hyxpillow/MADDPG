ca119f4809fec463339658068ba32492eae8604f
Hyxpillow <401887202@qq.com>
2022-01-30 19:29:59 +0800

    update agent.py main.py

--- agent.py
+++ agent.py
@@ -2,8 +2,6 @@ class Agent:
     # 具有的方法包括：
     #    构造函数
     #    学习
-    #    把loss发送给server
-    #    从server接受新的critic网络参数
     def __init__(self):
         # 成员变量如下：
         #    一个memory
@@ -12,10 +10,4 @@ class Agent:
         pass
 
     def learn(self):
-        pass
-
-    def send_server(self):
-        pass
-
-    def recv_server(self):
         pass
--- main.py
+++ main.py
@@ -1,15 +1,34 @@
-import time
+import agent
+import server
 import gym
-env = gym.make('CartPole-v0') #导入CartPole-v0游戏
-for episode in range(1000):  #跑Episodes幕，一幕相当于一次完整游戏
-	state = env.reset() #初始化环境，得到第一步状# 态
-	for step in range(200): #每一局游戏至多跑STEPS步
-		env.render()
-		action = 0 #强化学习算法，根据状态生成行动
-		state, reward, done, info = env.step(action) #根据action将游戏向前推进
-		# 把 (状态 奖励 动作 下一个状态) 存入 memory
-		# if step % 50 == 0:
-            # learn()
-		if done: # 如果游戏结束，则跳出step循环，进入下一episode；否则，继续step
-			break
+
+# 在执行MADDPG算法之前进行相关变量的初始化
+env = gym.make('CartPole-v0')
+M = 1000
+MAX_EPISODE_LENGTH = 200
+N = 10
+GAMMA = 0.05
+state_list = [0 for i in range(N)]
+agent_list = [agent.Agent() for i in range(N)]
+center_server = server.Server()
+
+# 真正开始执行MADDPG
+for episode in range(M):
+	state = env.reset()
+	state_list = [state] * N
+	for step in range(MAX_EPISODE_LENGTH):
+		# env.render()
+		for i, agent_i in enumerate(agent_list):
+			action = agent_i.actor(state_list[i])
+			state_, reward, done, info = env.step(action)
+			agent_i.store(state_list[i], action, reward, state_)
+			state_list[i] = state_
+		for i, agent_i in enumerate(agent_list):
+			state, action, reward, state_ = agent_i.sample()
+			action_ = agent_i.target_actor(state_)
+			y = reward + GAMMA * agent_i.target_critic(state_, action_)
+
+			center_server.recv_agent()
+		# if done: # 如果游戏结束，则跳出step循环，进入下一episode；否则，继续step
+		# 	break
 env.close() #结束游戏，关闭环境
