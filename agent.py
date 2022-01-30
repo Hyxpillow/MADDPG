1e13783ae9970840f9e08356e4cfe25681e7afb3
Jachin-jiang <jachin.jiang@gmail.com>
2022-01-30 23:59:40 +0800

    change agent.py main.py && add networks.py

--- agent.py
+++ agent.py
@@ -1,34 +1,84 @@
+import torch as T
+from networks import ActorNetwork, CriticNetwork
+##每个Agent都有一个buffer
+from memory import Memory
+
 class Agent:
-    def __init__(self):
+    def __init__(self, actor_dims, critic_dims, n_actions, n_agents, agent_idx, chkpt_dir,
+                    alpha=0.01, beta=0.01, fc1=64, 
+                    fc2=64, gamma=0.95, tau=0.01):
         # 成员变量如下：
         #    一个memory
         #    一个critic网络
         #    一个actor网络
-        pass
+        self.gamma = gamma
+        self.tau = tau
+        self.n_actions = n_actions
+        self.agent_name = 'agent_%s' % agent_idx
+        self.actor_network = ActorNetwork(alpha, actor_dims, fc1, fc2, n_actions, 
+                                  chkpt_dir=chkpt_dir,  name=self.agent_name+'_actor')
+        self.critic_network = CriticNetwork(beta, critic_dims, 
+                            fc1, fc2, n_agents, n_actions, 
+                            chkpt_dir=chkpt_dir, name=self.agent_name+'_critic')
+        self.target_actor_network = ActorNetwork(alpha, actor_dims, fc1, fc2, n_actions,
+                                        chkpt_dir=chkpt_dir, 
+                                        name=self.agent_name+'_target_actor')
+        self.target_critic_network = CriticNetwork(beta, critic_dims, 
+                                            fc1, fc2, n_agents, n_actions,
+                                            chkpt_dir=chkpt_dir,
+                                            name=self.agent_name+'_target_critic')
+        # 相关输入参数需要重新制定
+        self.memory = Memory(1000000, critic_dims, actor_dims, 
+                        n_actions, batch_size=1024)
+        self.update_network_parameters(tau=1)       
 
     def actor(self, state):  # 根据状态state，给出一个确定的action
-        pass
+        state = T.tensor([state], dtype=T.double).to(self.actor_network.device)
+        action = self.actor_network.forward(state)
+        return action
 
     def target_actor(self, state):  # 根据状态state，给出一个确定的action
-        pass
+        state = T.tensor([state], dtype=T.double).to(self.actor_network.device)
+        action = self.target_actor_network.forward(state)
+        return action
+
 
     def critic(self, state, action):  # 在状态state下，给action打分，
-        pass
+        state = T.tensor([state], dtype=T.double).to(self.actor_network.device)
+        val = self.critic_network.forward(state, action)
+        return val
 
     def target_critic(self, state, action):  # 在状态state下，给action打分
-        pass
+        state = T.tensor([state], dtype=T.double).to(self.actor_network.device)
+        val = self.target_critic_network.forward(state, action)
+        return val
 
-    def update_actor(self, loss):  # 更新actor网络
-        pass
+    # 注释修改了  更新actor网络 -> 更新target_actor网络
+    def update_target_actor(self, TAU):  # 更新target_actor网络
+        target_actor_params = self.target_actor_network.named_parameters()
+        actor_params = self.actor_network.named_parameters()
+        target_actor_state_dict = dict(target_actor_params)
+        actor_state_dict = dict(actor_params)
+        for name in actor_state_dict:
+            actor_state_dict[name] = TAU*actor_state_dict[name].clone() + \
+                    (1-TAU)*target_actor_state_dict[name].clone()
+        self.target_actor_network.load_state_dict(actor_state_dict)
 
     def get_target_critic_parameter(self):  # 取得target_critic网络的参数
-        pass
+        target_critic_params = self.target_critic_network.named_parameters()
+        target_critic_state_dict = dict(target_critic_params)
+        return target_critic_state_dict
 
-    def set_target_critic_parameter(self):  # 设置target_critic网络的参数
-        pass
+
+    def set_critic_parameter(self, theta):  # 设置critic网络的参数
+        self.critic_network.load_state_dict(theta)
+
+
+    def set_target_critic_parameter(self, theta_):  # 设置target_critic网络的参数
+        self.target_critic_network.load_state_dict(theta_)
 
     def store(self, state, action, reward, state_):  # 把(s, a, r, s_)存入memory
-        pass
+        self.memory.save(state, action, reward, state_)
 
     def sample(self):  # 随机返回一个(s, a, r, s_)
-        pass
+        return self.memory.sample()
--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import agent
 import server
 import gym
 
+# 计算loss需要用
+########################
+import torch 
+########################*/
+
+
 # 在执行MADDPG算法之前进行相关变量的初始化
 env = gym.make('CartPole-v0')
 M = 1000
@@ -25,6 +31,15 @@ for episode in range(M):
 			state_, reward, done, info = env.step(action)
 			agent_i.store(state_list[i], action, reward, state_)
 			state_list[i] = state_
+
+
+# 增加1.10特性，统一梯度清0
+############################################################
+		for i, agent_i in enumerate(agent_list):
+			agent_i.actor.optimizer.zero_grad()
+############################################################*/
+
+
 		for agent_i in agent_list:
 			state, action, reward, state_ = agent_i.sample()
 			action_ = agent_i.target_actor(state_)
@@ -32,13 +47,43 @@ for episode in range(M):
 			y = agent_i.critic(state, action)
 			center_server.recv(agent_i, (y - y_)**2)
 
-			actor_loss = agent_i.critic(state, action)
-			agent_i.update_actor(actor_loss)
+
+# pytorch1.10里面要先统一清0梯度，再运行，再统一step
+# 而且不确定传入actor_loss行不行，因为actor_loss好像不是一个值
+############################################################
+			actor_loss = agent_i.critic.forward(state, action)
+			actor_loss = -torch.mean(actor_loss)
+			actor_loss.backward(retain_graph=True)
+
+			# 原代码
+			# actor_loss = agent_i.critic(state, action)
+			# agent_i.update_actor(actor_loss)
+############################################################*/
+
+
+
+# 增加1.10特性，统一step
+############################################################
+		for i, agent_i in enumerate(agent_list):
+			agent_i.actor.optimizer.step()
+############################################################*/
+
+
 
 		center_server.update_critic()
-
 		theta = center_server.send()
+
+
+# 增加更新agent的critic网络和target_actor网络
+############################################################
+		# 可能出现的问题，梯度未清零，只是猜测，以便以后debug
+		for agent_i in agent_list:
+			agent_i.set_critic_parameter(theta)
+			agent_i.update_target_actor(TAU)
+############################################################*/
+
 		theta_ = agent_list[0].get_target_critic_parameter()
+		# 待修改，因为存在名字对应问题
 		theta_ = TAU * theta + (1 - TAU) * theta_
 		for agent_i in agent_list:
 			agent_i.set_target_critic_parameter(theta_)
